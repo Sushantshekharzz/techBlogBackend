9d46f5d5c7fc973f63fdbf5cbaf05426087706b6
Sushant <sushantshekhar01011999@gmail.com>
2024-06-24 02:05:12 +0530

    changess

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../model/user');
+var jwt = require('jsonwebtoken');
 
 
 /* GET home page. */
+// const secretKey = "Sushant Project"
 router.post('/', async function (req, res, next) {
   const userName = req.body.userName;
   const password = req.body.password;
+  const data  = {
+    "UserName":userName,
+    "Password"  : password
+  }
   try {
     const user = await User.findOne({ where: { username: userName, password: password } });
-
+    // const token  = jwt.sign(data,secretKey, {expiresIn:'1h'})
+    // console.log("token", token)
     if (user) {
       res.send({ 'message': "Sucessfully Login" , 'user':user})
     }
